640cb1904af17d81c668ef3f3acc06ab7abf3bab
Vasyl Habalevych <vasylhab@gmail.com>
2021-05-18 22:44:38 +0200

    Updates

---
+++ precache-manifest.1813b4f7b144554bae52f88206a99800.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39dc80c67dd9d7f4589186ebe98a655d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "187532660c6cd8a0b45d",
+    "url": "./static/css/main.1348c184.chunk.css"
+  },
+  {
+    "revision": "61e7aeb5a2e3db6ff37f",
+    "url": "./static/js/2.d1ce54ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d1ce54ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "187532660c6cd8a0b45d",
+    "url": "./static/js/main.694881aa.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
